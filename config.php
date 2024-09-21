cefaa8b394b6187ca9f5a410c46988e5dacce22a
Rhnspd <reyhanspeed2020@gmail.com>
2024-09-21 12:03:04 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Change this token with your bot token. You can get it from @botfather (https://t.me/botfather)
+ * also change this chat_id with your chat_id. To get your chat_id, you can
+ * use @id_chat_bot (https://t.me/id_chat_bot) in telegram. DONT FORGET TO START THE BOT.   
+ */
+$config = [
+    'telegram_bot_token' => '5669751939:AAFgBuZLq7zlT89Hshll0YXLjD_mjc_7GXc',
+    'chat_id'            => 5510246221
+];
